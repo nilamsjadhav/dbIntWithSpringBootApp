c1019c4195164ee9e57d17a7224830b02069a8ab
Nilam Jadhav <99525537+nilamsjadhav@users.noreply.github.com>
2023-04-04 20:16:43 +0530

    Modify return type of endpoints

--- src/main/kotlin/com/example/databaseExp/controller/NotesController.kt
+++ src/main/kotlin/com/example/databaseExp/controller/NotesController.kt
@@ -5,29 +5,27 @@ import com.example.databaseExp.dto.Response
 import com.example.databaseExp.model.Note
 import com.example.databaseExp.service.NotesService
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-/** GET -> to display notes,
- * POST -> to add notes
- */
 @RestController
 @RequestMapping("/notes")
 class NotesController(private val notesService: NotesService) {
 
   @GetMapping("/get-notes")
-  fun getNotes(): MutableIterable<Note> {
-    return notesService.get()
+  fun getNotes(): ResponseEntity<MutableIterable<Note>> {
+    return ResponseEntity.ok().body(notesService.get())
   }
 
   @PostMapping("/add-note", produces = [(MediaType.APPLICATION_JSON_VALUE)])
-  fun addNote(@RequestBody note: NoteLabel): Response {
+  fun addNote(@RequestBody note: NoteLabel): ResponseEntity<Response> {
     notesService.save(note)
-    return Response("Note add successfully")
+    return ResponseEntity.ok(Response("Note add successfully"))
   }
 
   @DeleteMapping("/remove-notes")
-  fun removeNotes(): Response {
+  fun removeNotes(): ResponseEntity<Response> {
     notesService.remove()
-    return Response("Removed all notes successfully")
+    return ResponseEntity.ok(Response("Removed all notes successfully"))
   }
 }
